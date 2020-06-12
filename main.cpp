09b712f96053e8db77d9df7ce5d67cec2ed63b7c
samuel cognolato <samuel.cognolato@gmail.com>
2020-06-12 17:34:04 +0100

    Si vola

--- main.cpp
+++ main.cpp
@@ -1,150 +1,183 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/xfeatures2d.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
 
 
 using namespace std;
 using namespace cv;
 
-int H, S, V, Hrange, Srange, Vrange;
+vector<Mat> loadImages(cv::String images_path) {
 
-int canny_threshold;
 
-int morph_size;
-int blur_size, sigma_range, sigma_space;
+    vector<String> filenames;   // vector of all the filenames found in the specified folder
+    vector<string> allowedExtensions = { ".jpg", ".jpeg", ".png", ".bmp" };    // allowed extensions for images
 
-int canny_low, canny_high;
+    /// for each extension, search for images in the specified folder
+    for (int i = 0; i < allowedExtensions.size(); i++) {
+        vector<String> currentFilenames;    // filenames found with the i-th extension
 
-void onChange(int, void * info) {
-    Mat img = (*(Mat *) info).clone();
+        /// look for matches with the pattern
+        cv::glob(images_path + "*" + allowedExtensions[i],
+            currentFilenames, true);
 
-    for (int r=0; r<img.rows; r++) {
-        for(int c=0; c<img.cols; c++) {
-            Vec3b & color = img.at<Vec3b>(r, c);
-            if (color[0]<H-Hrange || color[0]>H+Hrange) color[0]=0;
-            if (color[1]<S-Srange || color[1]>S+Srange) color[1]=0;
-            if (color[2]<V-Vrange || color[2]>V+Vrange) color[2]=0;
-        }
+        /// insert found filenames in the general vector
+        filenames.insert(
+            filenames.end(),
+            currentFilenames.begin(),
+            currentFilenames.end());
     }
-    while(img.cols>800 || img.rows>600)
-        resize(img, img, Size(img.cols/2, img.rows/2));
 
-    Mat hsv_channels[3];
-    cv::split( img, hsv_channels );
-    img = hsv_channels[2];
+    if (filenames.empty()) cout << "No images found. Exiting." << endl;
 
-    Mat temp = img.clone();
+    vector<Mat> images;
 
-    bilateralFilter( temp, img, blur_size*2+1, sigma_range, sigma_space);
-    //if (canny_threshold<1) canny_threshold = 1;
-    if (canny_low<1) canny_low = 1;
-    if (canny_high<1) canny_high = 1;
-    /// Canny detector
-    Canny(img, img, canny_low,
-          canny_high, 3 );
-    /*Mat kernel1 = Mat::ones(4, 4, CV_8U);
-    dilate(img, img, kernel1);
-
-
-    //Mat element = getStructuringElement( 2, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
-    Mat element = getStructuringElement( 0, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( 1, 1 ) );
-    morphologyEx( img, img, 2, element );*/
-
-    /*** develop mask ***/
-    int temp_H = 13;
-    int temp_Hrange = 35;
-    int temp_S = 24;
-    int temp_Srange = 256;
-    int temp_V = 15;
-    int temp_Vrange = 256;
-    int temp_canny_low = 173;
-    int temp_canny_high = 943;
-    int temp_blur_size = 15;
-    int temp_sigma_range = 24;
-    int temp_sigma_space = 25;
-
-    Mat mask = (*(Mat *) info).clone();
-    for (int r=0; r<mask.rows; r++) {
-        for(int c=0; c<mask.cols; c++) {
-            Vec3b & color = mask.at<Vec3b>(r, c);
-            if (color[0]<temp_H-temp_Hrange || color[0]>temp_H+temp_Hrange) color[0]=0;
-            if (color[1]<temp_S-temp_Srange || color[1]>temp_S+temp_Srange) color[1]=0;
-            if (color[2]<temp_V-temp_Vrange || color[2]>temp_V+temp_Vrange) color[2]=0;
-        }
+    /// load the images corresponding to each filename
+    for (int i = 0; i < filenames.size(); i++)
+    {
+        Mat img;
+        img = imread(filenames[i]);
+        while (img.cols > 800 || img.rows > 600)
+            resize(img, img, Size(img.cols / 2, img.rows / 2));
+        images.push_back(img);
     }
-    while(mask.cols>800 || mask.rows>600)
-        resize(mask, mask, Size(mask.cols/2, mask.rows/2));
 
-    cv::split( mask, hsv_channels );
-    mask = hsv_channels[2];
+    return images;
+}
 
-    temp = mask.clone();
+Mat createDictionary(String samples_path)
+{
+    //Step 1 - Obtain the set of bags of features.
 
-    bilateralFilter( temp, mask, temp_blur_size*2+1, temp_sigma_range, temp_sigma_space);
+    //to store the input file names
 
-    /// Canny detector
-    Canny(mask, mask, temp_canny_low,
-          temp_canny_high, 3 );
+    vector<Mat> sample_imgs = loadImages(samples_path);
 
+    //to store the current input image
+    Mat input;
+    //To store the keypoints that will be extracted by SIFT
+    vector<KeyPoint> keypoints;
+    //To store the SIFT descriptor of current image
+    Mat descriptor;
+    //To store all the descriptors that are extracted from all the images.
+    Mat featuresUnclustered;
+    //The SIFT feature extractor and descriptor
+    Ptr<xfeatures2d::SIFT> detector = xfeatures2d::SIFT::create();
+
+    //I select 20 (1000/50) images from 1000 images to extract
+    //feature descriptors and build the vocabulary
+    for (int f = 0; f < sample_imgs.size(); f++) {
+
+        cvtColor(sample_imgs[f], input, COLOR_BGR2GRAY);
+        //detect feature points
+        detector->detect(input, keypoints);
+        //compute the descriptors for each keypoint
+        detector->compute(input, keypoints, descriptor);
+        //put the all feature descriptors in a single Mat object 
+        featuresUnclustered.push_back(descriptor);
+        //print the percentage
+        printf("%i percent done\n", (int)((float)f / sample_imgs.size() * 100));
+    }
+
+    //Construct BOWKMeansTrainer
+    //the number of bags
+    int dictionarySize = 200;
+    //define Term Criteria
+    TermCriteria tc(TermCriteria::Type::MAX_ITER, 100, 0.001);
+    //retries number
+    int retries = 1;
+    //necessary flags
+    int flags = KMEANS_PP_CENTERS;
+    //Create the BoW (or BoF) trainer
+    BOWKMeansTrainer bowTrainer(dictionarySize, tc, retries, flags);
+    //cluster the feature vectors
+    Mat dictionary = bowTrainer.cluster(featuresUnclustered);
+
+    return dictionary;
+}
+
+void detectTrees(String test_path, Mat dictionary)
+{
+    //Step 2 - Obtain the BoF descriptor for given image/video frame. 
+
+    //create a nearest neighbor matcher
+    Ptr<DescriptorMatcher> matcher(new FlannBasedMatcher);
+    //create Sift feature point detector
+    Ptr<xfeatures2d::SIFT> detector = xfeatures2d::SIFT::create();
+    //create BoF (or BoW) descriptor extractor
+    BOWImgDescriptorExtractor bowDE(detector, matcher);
+    //Set the dictionary with the vocabulary we created in the first step
+    bowDE.setVocabulary(dictionary);
+
+    vector<Mat> test_imgs = loadImages(test_path);
+
+    //read the image
+    Mat img;
+    cvtColor(test_imgs[4], img, COLOR_BGR2GRAY);
+    //To store the keypoints that will be extracted by SIFT
+    vector<KeyPoint> keypoints;
+
+
+    int min_size = min(img.rows, img.cols);
+    int step = 5;
     
+    for (int scale = 1; scale < 10; scale++)
+    {
+        int w_size = min_size / scale;
 
-    imshow("temp", img);
+        for (int x = 0; x <= img.cols - w_size; x += step)
+        {
+            for (int y = 0; y <= img.rows - w_size; y += step)
+            {
+
+                Rect window(x, y, w_size, w_size);
+
+                //Detect SIFT keypoints (or feature points)
+                detector->detect(img(window), keypoints);
+                //To store the BoW (or BoF) representation of the image
+                Mat histogram;
+                //extract BoW (or BoF) descriptor from given image
+
+                bowDE.compute(img, keypoints, histogram);
+
+                float max = 0;
+
+                for (int word = 0; word < histogram.cols; word++)
+                {
+                    float value = histogram.at<float>(0, word);
+                    if (value > max) max = value;
+                }
+
+                cout << window << "; max = " << max << endl;
+
+                Mat canvas = img.clone();
+
+                rectangle(canvas, window, { 0, 0, 255 });
+
+                imshow("Window", canvas);
+
+                waitKey(1);
+
+            }
+        }
+    }
+    
 
 }
 
-int main(int args, char ** argv) {
+int main(int argc, const char* argv[])
+{
+    
+    String samples_path = "../../dataset/";
+    String test_path = "../../Benchmark_step_1/";
 
-    H = 13;
-    Hrange = 35;
-    S = 24;
-    Srange = 256;
-    V = 15;
-    Vrange = 256;
-    canny_threshold = 98;
-
-    morph_size = 3;
-    blur_size  =1;
-
-    //Mat img = imread("../train/17.jpg");
-    for (int i=1; i<10; i++) {
-        Mat img = imread("../Benchmark_step_1/Figure " + to_string(i) + ".jpg");
-        //Mat img = imread("../dataset/01.jpg");
-        //Mat img = imread(argv[1]);
-
-        //cvtColor(img, img, COLOR_BGR2YCrCb);
-        cvtColor(img, img, COLOR_BGR2HSV);
-
-        namedWindow("temp", WINDOW_AUTOSIZE);
-
-        /*createTrackbar("H threshold", "temp", &H, 256, onChange, &img);
-        createTrackbar("Hrange", "temp", &Hrange, 256, onChange, &img);*/
-
-        createTrackbar("S threshold", "temp", &S, 256, onChange, &img);
-        createTrackbar("Srange", "temp", &Vrange, 256, onChange, &img);
-
-        /*createTrackbar("V threshold", "temp", &V, 256, onChange, &img);
-        createTrackbar("Vrange", "temp", &Srange, 256, onChange, &img);*/
-
-        //createTrackbar("Canny_thr", "temp", &canny_threshold, 256, onChange, &img);
-        createTrackbar("Canny_low", "temp", &canny_low, 2000, onChange, &img);
-        createTrackbar("Canny_high", "temp", &canny_high, 2000, onChange, &img);
-
-        //createTrackbar("Morph size", "temp", &morph_size, 6, onChange, &img);
-        createTrackbar("blur size", "temp", &blur_size, 50, onChange, &img);
-        createTrackbar("sigma range", "temp", &sigma_range, 100, onChange, &img);
-        createTrackbar("sigma space", "temp", &sigma_space, 100, onChange, &img);
-
-        onChange(0, &img);
-
-        //imshow("temp", img);
-        waitKey(0);
-        destroyAllWindows();
-    }
-
-
-
-
-
+    Mat dictionary = createDictionary(samples_path);
+    detectTrees(test_path, dictionary);
 
     return 0;
 }
+
+

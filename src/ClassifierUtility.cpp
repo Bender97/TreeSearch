cddd39d726bc7deed80b7f9f22a610c698249495
samuel cognolato <samuel.cognolato@gmail.com>
2020-06-14 15:47:42 +0100

    Added skeleton

---
+++ src/ClassifierUtility.cpp
@@ -0,0 +1,15 @@
+//
+// Created by cogny on 14/06/20.
+//
+
+#include "../include/ClassifierUtility.h"
+
+double trainModel(string train_dataset_path, Ptr<ml::SVM> svm)
+{
+	return 0;
+}
+
+double testModel(string test_dataset_path, Ptr<ml::SVM> svm)
+{
+	return 0;
+}
---
+++ src/DatasetUtility.cpp
@@ -0,0 +1,15 @@
+//
+// Created by cogny on 14/06/20.
+//
+
+#include "../include/DatasetUtility.h"
+
+void buildTrainingSet(string input_images_path, Mat vocabulary, string output_images_path, float proportion)
+{
+	return;
+}
+
+Mat loadDataset(string dataset_path)
+{
+	return Mat();
+}

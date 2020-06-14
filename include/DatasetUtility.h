cc32cf55b126babd9e979a990cce2fcd4aad44bc
samuel cognolato <samuel.cognolato@gmail.com>
2020-06-14 17:46:18 +0100

    build implemented

--- include/DatasetUtility.h
+++ include/DatasetUtility.h
@@ -6,15 +6,25 @@
 #define BOF_DATASETUTILITY_H
 
 #include <iostream>
+#include <random>
+#include <algorithm>
 #include <opencv2/opencv.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
 #include "FilesUtility.h"
 #include "ImagesUtility.h"
+#include "ClassifierUtility.h"
+
+#define TRAINING_SET_DIR "hist_dataset_train.csv"
+#define TEST_SET_DIR "hist_dataset_test.csv"
+
+#define TREE_DIR "/tree"
+#define NON_TREE_DIR "/non_tree"
 
 using namespace std;
 using namespace cv;
 
-void buildTrainingSet(string input_images_path, Mat vocabulary, string output_images_path, float proportion);
-Mat loadDataset(string dataset_path);
+void buildTrainingSet(string input_images_path, Mat vocabulary, string output_CSVs_path, float proportion);
+pair<Mat, Mat> loadDataset(string dataset_path);
 
 #endif //BOF_DATASETUTILITY_H
--- src/DatasetUtility.cpp
+++ src/DatasetUtility.cpp
@@ -4,12 +4,82 @@
 
 #include "../include/DatasetUtility.h"
 
-void buildTrainingSet(string input_images_path, Mat vocabulary, string output_images_path, float proportion)
+void buildTrainingSet(string input_images_path, Mat vocabulary, string output_CSVs_path, float proportion)
 {
-	return;
+
+    // load images from tree and non_tree directories
+	vector<string> tree_images_paths = loadImagesPaths(input_images_path + TREE_DIR);
+    vector<string> non_tree_images_paths = loadImagesPaths(input_images_path + NON_TREE_DIR);
+
+    int n_tree = tree_images_paths.size();
+    int n_non_tree = non_tree_images_paths.size();
+
+    int n_images = n_tree + n_non_tree;
+
+    vector<pair<string, int>> images_paths(n_images);
+
+    // push all the images in the general vector together with their class
+    for (int i = 0; i < n_tree; i++)
+    {
+        images_paths.push_back(pair<string, int>(tree_images_paths[i], TREE_CLASS));
+    }
+    for (int i = 0; i < n_non_tree; i++)
+    {
+        images_paths.push_back(pair<string, int>(non_tree_images_paths[i], NON_TREE_CLASS));
+    }
+
+    auto rd = std::random_device{};
+    auto rng = std::default_random_engine{ rd() };
+    std::shuffle(begin(images_paths), end(images_paths), rng);
+
+    //create a nearest neighbor matcher
+    Ptr<DescriptorMatcher> matcher(new FlannBasedMatcher);
+    //create Sift feature point detector
+    Ptr<xfeatures2d::SIFT> detector = xfeatures2d::SIFT::create();
+    //create BoF (or BoW) descriptor extractor
+    BOWImgDescriptorExtractor bowDE(detector, matcher);
+    //Set the dictionary with the vocabulary we created in the first step
+    bowDE.setVocabulary(vocabulary);
+
+    ofstream train_set_f = prepareCSV(output_CSVs_path + TRAINING_SET_DIR);
+    ofstream test_set_f  = prepareCSV(output_CSVs_path + TEST_SET_DIR);
+
+    //To store the keypoints that will be extracted by SIFT
+    vector<KeyPoint> keypoints;
+
+    for (int i = 0; i < n_images; i++)
+    {
+        Mat img = imread(images_paths[i].first);
+        int img_class = images_paths[i].second;
+
+        cvtColor(img, img, COLOR_BGR2GRAY);
+
+        //Detect SIFT keypoints (or feature points)
+        detector->detect(img, keypoints);
+        //To store the BoW (or BoF) representation of the image
+        Mat histogram;
+        //extract BoW (or BoF) descriptor from given image
+        bowDE.compute(img, keypoints, histogram);
+
+        // insert the first n_images * proportion images into the train set, the rest into the test set
+        if (i < n_images * proportion)
+        {
+            addRowCSV(train_set_f, histogram, img_class);
+        }
+        else
+        {
+            addRowCSV(test_set_f, histogram, img_class);
+        }
+
+    }
+    
+    train_set_f.close();
+    test_set_f.close();
+
+
 }
 
-Mat loadDataset(string dataset_path)
+pair<Mat, Mat> loadDataset(string dataset_path)
 {
-	return Mat();
+	return pair<Mat, Mat>();
 }

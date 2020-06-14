54f64ff9f64498a1d64c51e07c9c28f1358b767e
Bender97 <bopallino@gmail.com>
2020-06-14 21:22:31 +0200

    svm predict simply works

--- src/ClassifierUtility.cpp
+++ src/ClassifierUtility.cpp
@@ -4,14 +4,16 @@
 
 #include "../include/ClassifierUtility.h"
 
-double trainModel(string train_dataset_path, Ptr<ml::SVM> svm)
+double trainModel(string train_dataset_path, Ptr<ml::SVM> &svm)
 {
     pair<Mat, Mat> dataset = loadDataset(train_dataset_path);
 
     svm = ml::SVM::create();
-    svm->setType(ml::SVM::C_SVC);
-    svm->setKernel(ml::SVM::LINEAR);
-    svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
+    svm->setType(ml::SVM::NU_SVC);
+    svm->setNu(0.5);
+    svm->setKernel(ml::SVM::RBF);
+
+    svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100000, 1e-6));
     svm->train(dataset.first, ml::ROW_SAMPLE, dataset.second);
 
     int error = 0;
@@ -38,10 +40,9 @@ double testModel(string test_dataset_path, Ptr<ml::SVM> svm)
     for (int r=0; r<dataset.first.rows; r++) {
         Mat hist = dataset.first.row(r);
 
-        Mat res;
         float response = svm->predict(hist);
         int result = round(response);
-        cout << "predicted: " << response << " actual: " << (int)dataset.second.at<uchar>(r) << endl;
+        //cout << "predicted: " << response << " actual: " << (int)dataset.second.at<uchar>(r) << endl;
         if (result!=dataset.second.at<int>(r))
             error++;
     }

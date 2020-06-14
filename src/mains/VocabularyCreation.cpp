1537a80471c43f1f50b6c155db40b2b70750c43b
Bender97 <bopallino@gmail.com>
2020-06-14 15:56:35 +0200

    update

---
+++ src/mains/VocabularyCreation.cpp
@@ -0,0 +1,13 @@
+//
+// Created by fusy on 14/06/20.
+//
+
+#include <iostream>
+using namespace std;
+
+#include "../../include/VocabularyUtility.h"
+
+int main(int args, char ** argv) {
+    cout << "Hello World" << endl;
+    return 0;
+}

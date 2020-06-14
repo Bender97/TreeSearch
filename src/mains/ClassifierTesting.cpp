c19960a95e50e0b57330f15a2e4b95bc8f6bb434
samuel cognolato <samuel.cognolato@gmail.com>
2020-06-14 15:58:59 +0100

    Added mains

---
+++ src/mains/ClassifierTesting.cpp
@@ -0,0 +1,13 @@
+//
+// Created by cogny on 14/06/20.
+//
+
+#include <iostream>
+using namespace std;
+
+#include "../../include/ClassifierUtility.h"
+
+int main(int args, char ** argv) {
+    cout << "Hello World" << endl;
+    return 0;
+}

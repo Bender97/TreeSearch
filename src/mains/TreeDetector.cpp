01eb2bf1fddd340f46b124f0dc20acc17589196d
Bender97 <bopallino@gmail.com>
2020-06-14 16:04:27 +0200

    up to date

---
+++ src/mains/TreeDetector.cpp
@@ -0,0 +1,15 @@
+//
+// Created by fusy on 14/06/20.
+//
+
+#include <iostream>
+using namespace std;
+
+#include "../../include/VocabularyUtility.h"
+#include "../../include/FilesUtility.h"
+#include "../../include/Detector.h"
+
+int main(int args, char ** argv) {
+    cout << "Hello World" << endl;
+    return 0;
+}
